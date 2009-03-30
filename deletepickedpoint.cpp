668c3795e366c3b7203dca2a9ac3bed8c0b65871
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-03-30 18:47:56 +0200

    point deletion now works with quads too, but produces deformed quads

--- deletepickedpoint.cpp
+++ deletepickedpoint.cpp
@@ -36,7 +36,7 @@ void DeletePickedPoint::foobar(vtkUnstructuredGrid *src, vtkUnstructuredGrid *ds
   int N_cells=src->GetNumberOfCells();
   
   //vector used to redefine the new point IDs
-  vector <vtkIdType> OffSet(N_points);
+  QVector <vtkIdType> OffSet(N_points);
   
   //count number of points and cells to remove + allocate + put mutated cells in a set
   int N_newpoints=-1;
@@ -98,29 +98,53 @@ void DeletePickedPoint::foobar(vtkUnstructuredGrid *src, vtkUnstructuredGrid *ds
     }
   };
   
+  cout<<"DeadCells="<<DeadCells<<endl;
+  cout<<"MutatedCells="<<MutatedCells<<endl;
+  cout<<"MutilatedCells="<<MutilatedCells<<endl;
+  
   //Copy undead cells
   for (vtkIdType id_cell = 0; id_cell < src->GetNumberOfCells(); ++id_cell) {//loop through src cells
     if(!DeadCells.contains(id_cell))//if the cell isn't dead
     {
-      vtkIdType N_pts, *src_pts, *dst_pts;
+      vtkIdType src_N_pts, *src_pts;
+      vtkIdType dst_N_pts, *dst_pts;
+      
       vtkIdType type_cell = src->GetCellType(id_cell);
-      src->GetCellPoints(id_cell, N_pts, src_pts);
-      src->GetCellPoints(id_cell, N_pts, dst_pts);
+      src->GetCellPoints(id_cell, src_N_pts, src_pts);
+      cout<<"-->id_cell="<<id_cell<<endl;
+      for(int i=0;i<src_N_pts;i++) cout<<"src_pts["<<i<<"]="<<src_pts[i]<<endl;
+//       src->GetCellPoints(id_cell, dst_N_pts, dst_pts);
+      dst_N_pts=src_N_pts;
+      dst_pts=new vtkIdType[dst_N_pts];
       if(MutatedCells.contains(id_cell))//mutated cell
       {
-        for(int i=0;i<N_pts;i++)
+        cout<<"processing mutated cell "<<id_cell<<endl;
+        for(int i=0;i<src_N_pts;i++)
         {
-          if(src_pts[i]==DeadNode) dst_pts[i]=SnapPoint-OffSet[SnapPoint];
+          if(src_pts[i]==DeadNode) {
+            cout<<"SnapPoint="<<SnapPoint<<endl;
+            cout<<"OffSet[SnapPoint]="<<OffSet[SnapPoint]<<endl;
+            cout<<"src_pts["<<i<<"]="<<src_pts[i]<<endl;
+            dst_pts[i]=SnapPoint-OffSet[SnapPoint];
+          }
           else dst_pts[i]=src_pts[i]-OffSet[src_pts[i]];
         }
+        cout<<"--->dst_pts:"<<endl;
+        for(int i=0;i<dst_N_pts;i++) cout<<"dst_pts["<<i<<"]="<<dst_pts[i]<<endl;
+        
       }
-      if(MutilatedCells.contains(id_cell))//mutilated cell
+      else if(MutilatedCells.contains(id_cell))//mutilated cell
       {
-        if(type_cell==VTK_QUAD) type_cell==VTK_TRIANGLE;
+        cout<<"processing mutilated cell "<<id_cell<<endl;
+        
+        if(type_cell==VTK_QUAD) {
+          type_cell=VTK_TRIANGLE;
+          dst_N_pts-=1;
+        }
         else {cout<<"FATAL ERROR: Unknown mutilated cell detected! It is not a quad! Potential xenomorph infestation!"<<endl;EG_BUG;}
         //merge points
         int j=0;
-        for(int i=0;i<N_pts;i++)
+        for(int i=0;i<src_N_pts;i++)
         {
           if(src_pts[i]==SnapPoint) { dst_pts[j]=SnapPoint-OffSet[SnapPoint];j++; }//SnapPoint
           else if(src_pts[i]!=DeadNode) { dst_pts[j]=src_pts[i]-OffSet[src_pts[i]];j++; }//pre-snap/dead + post-snap/dead
@@ -129,14 +153,23 @@ void DeletePickedPoint::foobar(vtkUnstructuredGrid *src, vtkUnstructuredGrid *ds
       }
       else//normal cell
       {
-        for(int i=0;i<N_pts;i++)
+        cout<<"processing normal cell "<<id_cell<<endl;
+        for(int i=0;i<src_N_pts;i++)
         {
           dst_pts[i]=src_pts[i]-OffSet[src_pts[i]];
         }
       }
       //copy the cell
-      vtkIdType id_new_cell = dst->InsertNextCell(type_cell, N_pts, dst_pts);
+      vtkIdType id_new_cell = dst->InsertNextCell(type_cell, dst_N_pts, dst_pts);
       copyCellData(src, id_cell, dst, id_new_cell);
+      cout<<"===Copying cell "<<id_cell<<" to "<<id_new_cell<<"==="<<endl;
+      cout<<"src_pts:"<<endl;
+      for(int i=0;i<src_N_pts;i++) cout<<"src_pts["<<i<<"]="<<src_pts[i]<<endl;
+      cout<<"dst_pts:"<<endl;
+      for(int i=0;i<dst_N_pts;i++) cout<<"dst_pts["<<i<<"]="<<dst_pts[i]<<endl;
+      cout<<"OffSet="<<OffSet<<endl;
+      cout<<"===Copying cell end==="<<endl;
+      delete dst_pts;
     }
   };
 }
