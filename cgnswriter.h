c081640b2b2450452e73b7977497e40e313c1daf
Oliver Gloth <ogloth@engits.com>
2009-04-16 11:56:58 +0200

    started CGNS output

---
+++ cgnswriter.h
@@ -0,0 +1,17 @@
+#ifndef CGNSWRITER_H
+#define CGNSWRITER_H
+
+class CgnsWriter;
+
+#include "iooperation.h"
+
+class CgnsWriter : public IOOperation
+{
+
+public:
+
+
+
+};
+
+#endif // CGNSWRITER_H
