6145450ae8a0342d8bd16aa0caae2bb172102993
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-05-20 10:09:45 +0200

    reenabled experimental features option

--- src/guimainwindow.cpp
+++ src/guimainwindow.cpp
@@ -270,8 +270,8 @@ GuiMainWindow::GuiMainWindow() : QMainWindow(NULL)
   
   N_chars = 0;
   
-  bool exp_features;
-//   getSet("","enable experimental features",false,exp_features);
+  bool exp_features=false;
+  getSet("","enable experimental features",false,exp_features);
   bool undo_redo;
   getSet("","enable undo/redo",false,undo_redo);
   bool undo_redo_mode;
