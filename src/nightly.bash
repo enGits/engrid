5dfe3964e47e147848edfebf63468eb0e84da513
Oliver Gloth <ogloth@engits.com>
2009-05-23 16:40:20 +0200

    added some comments

--- src/nightly.bash
+++ src/nightly.bash
@@ -2,3 +2,4 @@
 /usr/bin/doxygen Doxyfile
 ./checkcomments.py *.h *.cxx *.cpp math/*.h > comments.mail
 mailx -s "ENGRID: comments" ogloth@engits.com < comments.mail
+mailx -s "ENGRID: comments" mtaverne@engits.com < comments.mail
