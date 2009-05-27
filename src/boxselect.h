918a642e34484f66a9cf6b9356c797fae8ac7f51
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-05-27 11:10:39 +0200

    Added license headers

--- src/boxselect.h
+++ src/boxselect.h
@@ -1,13 +1,24 @@
 //
-// C++ Interface: boxselect
-//
-// Description: 
-//
-//
-// Author: Mike Taverne <mtaverne@engits.com>, (C) 2009
-//
-// Copyright: See COPYING file that comes with this distribution
-//
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +                                                                      +
+// + This file is part of enGrid.                                         +
+// +                                                                      +
+// + Copyright 2008,2009 Oliver Gloth                                     +
+// +                                                                      +
+// + enGrid is free software: you can redistribute it and/or modify       +
+// + it under the terms of the GNU General Public License as published by +
+// + the Free Software Foundation, either version 3 of the License, or    +
+// + (at your option) any later version.                                  +
+// +                                                                      +
+// + enGrid is distributed in the hope that it will be useful,            +
+// + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
+// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
+// + GNU General Public License for more details.                         +
+// +                                                                      +
+// + You should have received a copy of the GNU General Public License    +
+// + along with enGrid. If not, see <http://www.gnu.org/licenses/>.       +
+// +                                                                      +
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //
 #ifndef BOXSELECT_H
 #define BOXSELECT_H
@@ -15,9 +26,6 @@
 #include <operation.h>
 #include <vtkBoxWidget.h>
 
-/**
-	@author Mike Taverne <mtaverne@engits.com>
-*/
 class BoxSelect : public Operation
 {
 private:
