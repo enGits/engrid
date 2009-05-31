fec5811557386284cab013259099ed540f34d32a
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-05-31 11:58:56 +0200

    fixed rebuild.sh

--- src/testing/rebuild.sh
+++ src/testing/rebuild.sh
@@ -2,13 +2,13 @@
 set -eux
 
 echo "Building engrid.pro release version"
-make distclean && qmake engrid.pro && make -j2
+qmake && make distclean && qmake engrid.pro && make -j2
 echo "Building engrid.pro.cgns release version"
-make distclean && qmake engrid.pro.cgns && make -j2
+qmake && make distclean && qmake engrid.pro.cgns && make -j2
 
 echo "Building engrid.pro debug version"
-make distclean && qmake engrid.pro && make -j2 debug
+qmake && make distclean && qmake engrid.pro && make -j2 debug
 echo "Building engrid.pro.cgns debug version"
-make distclean && qmake engrid.pro.cgns && make -j2 debug
+qmake && make distclean && qmake engrid.pro.cgns && make -j2 debug
 
 echo "SUCCESS: Everything compiles."
