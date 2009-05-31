2ffc48c125ab071a36c63745bf121e07d0fcf6b9
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-05-31 12:19:23 +0200

    Added exit on error statements to rebuild.sh just in case

--- src/testing/rebuild.sh
+++ src/testing/rebuild.sh
@@ -2,13 +2,13 @@
 set -eux
 
 echo "Building engrid.pro release version"
-qmake && make distclean && qmake engrid.pro && make -j2
+qmake && make distclean && qmake engrid.pro && make -j2 || exit 1
 echo "Building engrid.pro.cgns release version"
-qmake && make distclean && qmake engrid.pro.cgns && make -j2
+qmake && make distclean && qmake engrid.pro.cgns && make -j2 || exit 1
 
 echo "Building engrid.pro debug version"
-qmake && make distclean && qmake engrid.pro && make -j2 debug
+qmake && make distclean && qmake engrid.pro && make -j2 debug || exit 1
 echo "Building engrid.pro.cgns debug version"
-qmake && make distclean && qmake engrid.pro.cgns && make -j2 debug
+qmake && make distclean && qmake engrid.pro.cgns && make -j2 debug || exit 1
 
 echo "SUCCESS: Everything compiles."
