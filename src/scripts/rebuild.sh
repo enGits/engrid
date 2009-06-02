c10c7f94e04d005e232117ba501acf4272318f7d
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-06-02 10:18:04 +0200

    Added environment setup to rebuild.sh

--- src/scripts/rebuild.sh
+++ src/scripts/rebuild.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -eux
+
+#set up environment
 PATH=/usr/bin:/opt/shared/Qt/4.5.0/debug/bin:$PATH
+source ./scripts/setup_paths.cgns.sh engits
+
+echo "PATHS:"
+echo $QTDIR
+echo $VTKLIBDIR
+echo $VTKINCDIR
+echo $CGNSINCDIR
+echo $CGNSLIBDIR
+echo $LD_LIBRARY_PATH
 
 echo "Building engrid.pro release version"
 qmake && make distclean && qmake engrid.pro && make -j2 || exit 1
