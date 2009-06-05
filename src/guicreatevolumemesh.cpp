55d0bcd19fbbcc275c72c3f5ca13c359d43fb864
Oliver Gloth <ogloth@engits.com>
2009-06-05 19:21:36 +0200

    added multiple volume support

--- src/guicreatevolumemesh.cpp
+++ src/guicreatevolumemesh.cpp
@@ -37,10 +37,36 @@ void GuiCreateVolumeMesh::before()
 
 void GuiCreateVolumeMesh::operate()
 {
-  CreateVolumeMesh mesh_volume;
-  mesh_volume.setGrid(grid);
   QString volume_name = getSelectedVolume(ui.listWidget);
+  VolumeDefinition V = mainWindow()->getVol(volume_name);
+  CreateVolumeMesh mesh_volume;
+  EG_VTKSP(vtkUnstructuredGrid, part_grid);
+  EG_VTKSP(vtkUnstructuredGrid, rest_grid);
+  {
+    MeshPartition part(volume_name);
+    part.setVolumeOrientation();
+    part.extractToVtkGrid(part_grid);
+    MeshPartition rest(grid);
+    rest.setRemainder(part);
+    rest.extractToVtkGrid(rest_grid);
+  }
+  //writeGrid(part_grid, "forVolumeMesher");
+  mesh_volume.setGrid(part_grid);
   mesh_volume();
+  EG_VTKDCC(vtkIntArray, cell_code, part_grid, "cell_code");
+  for (vtkIdType id_cell = 0; id_cell < part_grid->GetNumberOfCells(); ++id_cell) {
+    if (isVolume(part_grid, id_cell)) {
+      cell_code->SetValue(id_cell, V.getVC());
+    }
+  }
+  makeCopy(part_grid, grid);
+  {
+    MeshPartition part(grid, true);
+    MeshPartition rest(rest_grid, true);
+    part.addPartition(rest);
+  }
+  resetOrientation(grid);
+  createIndices(grid);
 }
 
 
