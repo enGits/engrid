bca9cb39a2deef0b57a2acfff2c0f8c856a85cf1
Oliver Gloth <ogloth@engits.com>
2009-06-12 14:39:01 +0200

    now empty

--- src/laplacesmoother.cpp
+++ src/laplacesmoother.cpp
@@ -28,8 +28,7 @@
 
 using namespace GeometryTools;
 
-LaplaceSmoother::LaplaceSmoother()
-: SurfaceOperation()
+LaplaceSmoother::LaplaceSmoother() : SurfaceOperation()
 {
    DebugLevel=0;
    setQuickSave(true);
@@ -39,56 +38,5 @@ void LaplaceSmoother::operate()
 {
   cout<<"=== LaplaceSmoother START ==="<<endl;
   UpdateNodeType();
-  
-  QVector<vtkIdType> AllCells;
-  getAllSurfaceCells(AllCells, grid);
-  QVector<vtkIdType> SelectedCells;
-  getSurfaceCells(m_bcs, SelectedCells, grid);
-  
-  cout<<"setCells START"<<endl;
-  setCells(AllCells);
-  cout<<"setCells END"<<endl;
-  
-  QSet <vtkIdType> SelectedNodes;
-  getSurfaceNodes(m_bcs,SelectedNodes,grid);
-  
-  EG_VTKDCC(vtkIntArray, cell_code, grid, "cell_code");
-  EG_VTKDCN(vtkCharArray, node_type, grid, "node_type");
-  int moved_points=0;
-  
-  for(int i_iter=0;i_iter<NumberOfIterations;i_iter++)
-  {
-    foreach(vtkIdType id_nodeG,SelectedNodes)
-    {
-      if(node_type->GetValue(id_nodeG)!=VTK_FIXED_VERTEX)
-      {
-        vec3_t G(0,0,0);
-        QVector <vtkIdType> PSP = getPotentialSnapPoints(id_nodeG);
-        foreach(vtkIdType id_nodeM, PSP)
-        {
-          vec3_t M;
-          grid->GetPoint(id_nodeM, M.data());
-          G+=M;
-        }
-        G=(1./PSP.size())*G;
-        
-        vec3_t P;
-        P=project(G);
-        
-        //check that no cell gets flipped!
-        while(FlippedCells(id_nodeG,P))
-        {
-          vec3_t x0_old;
-          grid->GetPoint(id_nodeG, x0_old.data());
-          P=x0_old+0.5*(P-x0_old);
-        };
-        
-        grid->GetPoints()->SetPoint(id_nodeG, P.data());
-        
-        moved_points++;
-      }
-    }
-  }
-  
   cout<<"=== LaplaceSmoother END ==="<<endl;
 }
