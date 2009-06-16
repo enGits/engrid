7946d125738a5820f242a54157f2169383312ce5
Oliver Gloth <ogloth@engits.com>
2009-06-16 21:38:12 +0200

    neighbours implemented, nodes remaining

--- src/octree.cpp
+++ src/octree.cpp
@@ -22,6 +22,22 @@
 //
 #include "octree.h"
 
+OctreeCell::OctreeCell()
+{
+  for (int i = 0; i < 8; ++i) {
+    m_Node[i] = -1;
+    m_Child[i] = -1;
+  }
+  for (int i = 0; i < 6; ++i) {
+    m_Neighbour[i] = -1;
+  }
+  m_Parent = -1;
+  m_Level = 0;
+}
+
+
+
+
 Octree::Octree()
 {
   m_Origin = vec3_t(0,0,0);
@@ -29,6 +45,7 @@ Octree::Octree()
   m_Nodes.resize(8);
   m_Cells.resize(1);
   setBounds(vec3_t(0,0,0), vec3_t(1,1,1));
+  setSmoothTransitionOn();
 }
 
 void Octree::setOrigin(vec3_t x0)
@@ -51,6 +68,184 @@ void Octree::setBounds(vec3_t corner1, vec3_t corner2)
   }
   m_Corner1 = corner1;
   m_Corner2 = corner2;
+  m_Nodes[0].m_Position = vec3_t(corner1[0], corner1[1], corner1[2]);
+  m_Nodes[1].m_Position = vec3_t(corner2[0], corner1[1], corner1[2]);
+  m_Nodes[2].m_Position = vec3_t(corner1[0], corner2[1], corner1[2]);
+  m_Nodes[3].m_Position = vec3_t(corner2[0], corner2[1], corner1[2]);
+  m_Nodes[4].m_Position = vec3_t(corner1[0], corner1[1], corner2[2]);
+  m_Nodes[5].m_Position = vec3_t(corner2[0], corner1[1], corner2[2]);
+  m_Nodes[6].m_Position = vec3_t(corner1[0], corner2[1], corner2[2]);
+  m_Nodes[7].m_Position = vec3_t(corner2[0], corner2[1], corner2[2]);
+  for (int i = 0; i < 8; ++i) {
+    m_Cells[0].m_Node[i] = i;
+  }
 }
 
-
+void Octree::refineAll()
+{
+  int N1;
+  int N2;
+  do {
+    N1 = 0;
+    N2 = 0;
+    for (int cell = 0; cell < m_Cells.size(); ++cell) {
+      if (m_ToRefine[cell]) {
+        ++N1;
+        for (int face = 0; face < 6; ++face) {
+          int neigh = getNeighbour(cell, face);
+          if (neigh != -1) {
+            if (m_Cells[neigh].m_Level < m_Cells[cell].m_Level) {
+              m_ToRefine[neigh] = true;
+              ++N2;
+            }
+          }
+        }
+      }
+    }
+  } while (N2 > 0);
+  N2 = m_Cells.size();
+  m_Cells.insert(N2, N1, OctreeCell());
+  N1 = N2;
+  for (int cell = 0; cell < N1; ++cell) {
+    if (m_ToRefine[cell]) {
+      for (int child = 0; child < 8; ++child) {
+        m_Cells[cell].m_Child[child] = N2;
+        m_Cells[N2].m_Parent = cell;
+        m_Cells[N2].m_Level = m_Cells[cell].m_Level + 1;
+        ++N2;
+      }
+      // - - -
+      m_Cells[m_Cells[cell].m_Child[0]].m_Neighbour[1] = m_Cells[cell].m_Child[1];
+      m_Cells[m_Cells[cell].m_Child[0]].m_Neighbour[3] = m_Cells[cell].m_Child[2];
+      m_Cells[m_Cells[cell].m_Child[0]].m_Neighbour[5] = m_Cells[cell].m_Child[4];
+      // + - -
+      m_Cells[m_Cells[cell].m_Child[1]].m_Neighbour[0] = m_Cells[cell].m_Child[0];
+      m_Cells[m_Cells[cell].m_Child[1]].m_Neighbour[3] = m_Cells[cell].m_Child[3];
+      m_Cells[m_Cells[cell].m_Child[1]].m_Neighbour[5] = m_Cells[cell].m_Child[5];
+      // - + -
+      m_Cells[m_Cells[cell].m_Child[2]].m_Neighbour[1] = m_Cells[cell].m_Child[3];
+      m_Cells[m_Cells[cell].m_Child[2]].m_Neighbour[2] = m_Cells[cell].m_Child[0];
+      m_Cells[m_Cells[cell].m_Child[2]].m_Neighbour[5] = m_Cells[cell].m_Child[6];
+      // + + -
+      m_Cells[m_Cells[cell].m_Child[3]].m_Neighbour[0] = m_Cells[cell].m_Child[2];
+      m_Cells[m_Cells[cell].m_Child[3]].m_Neighbour[2] = m_Cells[cell].m_Child[1];
+      m_Cells[m_Cells[cell].m_Child[3]].m_Neighbour[5] = m_Cells[cell].m_Child[7];
+      // - - +
+      m_Cells[m_Cells[cell].m_Child[4]].m_Neighbour[1] = m_Cells[cell].m_Child[5];
+      m_Cells[m_Cells[cell].m_Child[4]].m_Neighbour[3] = m_Cells[cell].m_Child[6];
+      m_Cells[m_Cells[cell].m_Child[4]].m_Neighbour[4] = m_Cells[cell].m_Child[0];
+      // + - +
+      m_Cells[m_Cells[cell].m_Child[5]].m_Neighbour[0] = m_Cells[cell].m_Child[4];
+      m_Cells[m_Cells[cell].m_Child[5]].m_Neighbour[3] = m_Cells[cell].m_Child[7];
+      m_Cells[m_Cells[cell].m_Child[5]].m_Neighbour[4] = m_Cells[cell].m_Child[1];
+      // - + +
+      m_Cells[m_Cells[cell].m_Child[6]].m_Neighbour[1] = m_Cells[cell].m_Child[7];
+      m_Cells[m_Cells[cell].m_Child[6]].m_Neighbour[2] = m_Cells[cell].m_Child[4];
+      m_Cells[m_Cells[cell].m_Child[6]].m_Neighbour[4] = m_Cells[cell].m_Child[2];
+      // + + +
+      m_Cells[m_Cells[cell].m_Child[7]].m_Neighbour[0] = m_Cells[cell].m_Child[6];
+      m_Cells[m_Cells[cell].m_Child[7]].m_Neighbour[2] = m_Cells[cell].m_Child[5];
+      m_Cells[m_Cells[cell].m_Child[7]].m_Neighbour[4] = m_Cells[cell].m_Child[3];
+    }
+  }
+  for (int cell = 0; cell < N1; ++cell) {
+    if (m_Cells[cell].m_Child[0] >= N1) {
+      {
+        int neigh = m_Cells[cell].m_Neighbour[0];
+        if (neigh != -1) {
+          if (m_Cells[neigh].m_Child[0] != -1) {
+            m_Cells[m_Cells[cell].m_Child[0]].m_Neighbour[0] = m_Cells[neigh].m_Child[1];
+            m_Cells[m_Cells[cell].m_Child[2]].m_Neighbour[0] = m_Cells[neigh].m_Child[3];
+            m_Cells[m_Cells[cell].m_Child[4]].m_Neighbour[0] = m_Cells[neigh].m_Child[5];
+            m_Cells[m_Cells[cell].m_Child[6]].m_Neighbour[0] = m_Cells[neigh].m_Child[7];
+          } else {
+            m_Cells[m_Cells[cell].m_Child[0]].m_Neighbour[0] = neigh;
+            m_Cells[m_Cells[cell].m_Child[2]].m_Neighbour[0] = neigh;
+            m_Cells[m_Cells[cell].m_Child[4]].m_Neighbour[0] = neigh;
+            m_Cells[m_Cells[cell].m_Child[6]].m_Neighbour[0] = neigh;
+          }
+        }
+      }
+      {
+        int neigh = m_Cells[cell].m_Neighbour[1];
+        if (neigh != -1) {
+          if (m_Cells[neigh].m_Child[0] != -1) {
+            m_Cells[m_Cells[cell].m_Child[1]].m_Neighbour[1] = m_Cells[neigh].m_Child[0];
+            m_Cells[m_Cells[cell].m_Child[3]].m_Neighbour[1] = m_Cells[neigh].m_Child[2];
+            m_Cells[m_Cells[cell].m_Child[5]].m_Neighbour[1] = m_Cells[neigh].m_Child[4];
+            m_Cells[m_Cells[cell].m_Child[7]].m_Neighbour[1] = m_Cells[neigh].m_Child[6];
+          } else {
+            m_Cells[m_Cells[cell].m_Child[1]].m_Neighbour[1] = neigh;
+            m_Cells[m_Cells[cell].m_Child[3]].m_Neighbour[1] = neigh;
+            m_Cells[m_Cells[cell].m_Child[5]].m_Neighbour[1] = neigh;
+            m_Cells[m_Cells[cell].m_Child[7]].m_Neighbour[1] = neigh;
+          }
+        }
+      }
+      {
+        int neigh = m_Cells[cell].m_Neighbour[2];
+        if (neigh != -1) {
+          if (m_Cells[neigh].m_Child[0] != -1) {
+            m_Cells[m_Cells[cell].m_Child[0]].m_Neighbour[2] = m_Cells[neigh].m_Child[2];
+            m_Cells[m_Cells[cell].m_Child[1]].m_Neighbour[2] = m_Cells[neigh].m_Child[3];
+            m_Cells[m_Cells[cell].m_Child[4]].m_Neighbour[2] = m_Cells[neigh].m_Child[6];
+            m_Cells[m_Cells[cell].m_Child[5]].m_Neighbour[2] = m_Cells[neigh].m_Child[7];
+          } else {
+            m_Cells[m_Cells[cell].m_Child[0]].m_Neighbour[2] = neigh;
+            m_Cells[m_Cells[cell].m_Child[1]].m_Neighbour[2] = neigh;
+            m_Cells[m_Cells[cell].m_Child[4]].m_Neighbour[2] = neigh;
+            m_Cells[m_Cells[cell].m_Child[5]].m_Neighbour[2] = neigh;
+          }
+        }
+      }
+      {
+        int neigh = m_Cells[cell].m_Neighbour[3];
+        if (neigh != -1) {
+          if (m_Cells[neigh].m_Child[0] != -1) {
+            m_Cells[m_Cells[cell].m_Child[2]].m_Neighbour[3] = m_Cells[neigh].m_Child[0];
+            m_Cells[m_Cells[cell].m_Child[3]].m_Neighbour[3] = m_Cells[neigh].m_Child[1];
+            m_Cells[m_Cells[cell].m_Child[6]].m_Neighbour[3] = m_Cells[neigh].m_Child[4];
+            m_Cells[m_Cells[cell].m_Child[7]].m_Neighbour[3] = m_Cells[neigh].m_Child[5];
+          } else {
+            m_Cells[m_Cells[cell].m_Child[2]].m_Neighbour[3] = neigh;
+            m_Cells[m_Cells[cell].m_Child[3]].m_Neighbour[3] = neigh;
+            m_Cells[m_Cells[cell].m_Child[6]].m_Neighbour[3] = neigh;
+            m_Cells[m_Cells[cell].m_Child[7]].m_Neighbour[3] = neigh;
+          }
+        }
+      }
+      {
+        int neigh = m_Cells[cell].m_Neighbour[4];
+        if (neigh != -1) {
+          if (m_Cells[neigh].m_Child[0] != -1) {
+            m_Cells[m_Cells[cell].m_Child[0]].m_Neighbour[4] = m_Cells[neigh].m_Child[4];
+            m_Cells[m_Cells[cell].m_Child[1]].m_Neighbour[4] = m_Cells[neigh].m_Child[5];
+            m_Cells[m_Cells[cell].m_Child[2]].m_Neighbour[4] = m_Cells[neigh].m_Child[6];
+            m_Cells[m_Cells[cell].m_Child[3]].m_Neighbour[4] = m_Cells[neigh].m_Child[7];
+          } else {
+            m_Cells[m_Cells[cell].m_Child[0]].m_Neighbour[4] = neigh;
+            m_Cells[m_Cells[cell].m_Child[1]].m_Neighbour[4] = neigh;
+            m_Cells[m_Cells[cell].m_Child[2]].m_Neighbour[4] = neigh;
+            m_Cells[m_Cells[cell].m_Child[3]].m_Neighbour[4] = neigh;
+          }
+        }
+      }
+      {
+        int neigh = m_Cells[cell].m_Neighbour[5];
+        if (neigh != -1) {
+          if (m_Cells[neigh].m_Child[0] != -1) {
+            m_Cells[m_Cells[cell].m_Child[4]].m_Neighbour[5] = m_Cells[neigh].m_Child[0];
+            m_Cells[m_Cells[cell].m_Child[5]].m_Neighbour[5] = m_Cells[neigh].m_Child[1];
+            m_Cells[m_Cells[cell].m_Child[6]].m_Neighbour[5] = m_Cells[neigh].m_Child[2];
+            m_Cells[m_Cells[cell].m_Child[7]].m_Neighbour[5] = m_Cells[neigh].m_Child[3];
+          } else {
+            m_Cells[m_Cells[cell].m_Child[4]].m_Neighbour[5] = neigh;
+            m_Cells[m_Cells[cell].m_Child[5]].m_Neighbour[5] = neigh;
+            m_Cells[m_Cells[cell].m_Child[6]].m_Neighbour[5] = neigh;
+            m_Cells[m_Cells[cell].m_Child[7]].m_Neighbour[5] = neigh;
+          }
+        }
+      }
+    }
+  }
+}
--- src/octree.h
+++ src/octree.h
@@ -23,33 +23,59 @@
 #ifndef OCTREE_H
 #define OCTREE_H
 
+class Octree;
+
 #include "egvtkobject.h"
+
+class OctreeNode
+{
+
+  friend class Octree;
+
+  vec3_t m_Position;
+
+public:
+
+  vec3_t& getPosition()                { return m_Position; }
+  void    setPosition(const vec3_t& x) { m_Position = x; }
+
+};
+
+class OctreeCell
+{
+
+  friend class Octree;
+
+  int m_Node[8];
+  int m_Child[8];
+  int m_Neighbour[6];
+  int m_Parent;
+  int m_Level;
+
+public:
+
+  OctreeCell();
+
+  int  getNode     (int i) { return m_Node[i]; }
+  int  getNeighbour(int i) { return m_Neighbour[i]; }
+
+};
 
 class Octree : public EgVtkObject
 {
 
-private: // data-types
-
-  struct OctreeNode
-  {
-    vec3_t x;
-  };
-
-  struct OctreeCell
-  {
-    int n[8];
-  };
-
 private: // attributes
 
-  vec3_t m_Origin;  /// origin of internal coordinate system
-  mat3_t m_Base;    /// base vectors of internal coordinate system
-  mat3_t m_InvBase; /// inverted base of internal coordiante system
-  vec3_t m_Corner1; /// first corner of extend box of the whole domain (in internal coordinates)
-  vec3_t m_Corner2; /// second corner of extend box of the whole domain (in internal coordinates)
+  vec3_t m_Origin;  ///< origin of internal coordinate system
+  mat3_t m_Base;    ///< base vectors of internal coordinate system
+  mat3_t m_InvBase; ///< inverted base of internal coordiante system
+  vec3_t m_Corner1; ///< first corner of extend box of the whole domain (in internal coordinates)
+  vec3_t m_Corner2; ///< second corner of extend box of the whole domain (in internal coordinates)
+  bool   m_SmoothTransition;
 
   QVector<OctreeNode> m_Nodes;
   QVector<OctreeCell> m_Cells;
+  QVector<bool>       m_ToRefine;
 
 public: // methods
 
@@ -61,6 +87,13 @@ public: // methods
 
   vec3_t transfTo(vec3_t x);
   vec3_t transfFrom(vec3_t r);
+
+  int  getNeighbour(int cell, int neigh) { return m_Cells[cell].m_Neighbour[neigh]; }
+
+  void markToRefine(int cell) { m_ToRefine[cell] = true; }
+  void refineAll();
+  void setSmoothTransitionOn()  { m_SmoothTransition = true; }
+  void setSmoothTransitionOff() { m_SmoothTransition = false; }
 
 };
 
