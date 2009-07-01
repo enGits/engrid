09ebdd60f038d697b5c65a411b5e501dfdd35a8f
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-07-01 18:39:09 +0200

    cleanup

--- src/filetemplate.cpp
+++ src/filetemplate.cpp
@@ -9,16 +9,11 @@ using namespace std;
 int fileTemplateTest( int argc, char ** argv ) {
   QApplication app(argc, argv);
   
-  MultipleFileTemplate multiple_file_template;
-  multiple_file_template.addFile("/data1/home/mtaverne/engrid/src/resources/openfoam/simpleFoam/system/fvSchemes.template");
-  multiple_file_template.addFile("/data1/home/mtaverne/engrid/src/resources/openfoam/simpleFoam/system/fvSchemes2.template");
-//   multiple_file_template.addFile("/data1/home/mtaverne/engrid/src/resources/openfoam/simpleFoam/system/fvSchemes2.template");
+  QVector <QString> files;
+  files.push_back("/data1/home/mtaverne/engrid/src/resources/openfoam/simpleFoam/system/fvSchemes.template");
+  files.push_back("/data1/home/mtaverne/engrid/src/resources/openfoam/simpleFoam/system/fvSchemes2.template");
   
-//   GuiTemplateViewer gui_template_viewer("/data1/home/mtaverne/engrid/src/resources/openfoam/simpleFoam/system/fvSchemes.template");
-//   GuiTemplateViewer gui_template_viewer(multiple_file_template);
-//   gui_template_viewer.show();
-  
-  SuperGui super_gui(multiple_file_template);
+  SuperGui super_gui(files);
   super_gui.show();
   
   return app.exec();
@@ -66,6 +61,7 @@ int FileTemplate::open(QString filename) {
 
 int FileTemplate::save() {
   this->saveAs(m_FileInfo.completeBaseName());
+  return(0);
 }
 
 int FileTemplate::saveAs(QString filename) {
@@ -96,10 +92,8 @@ int FileTemplate::process() {
   QStringList L_open = m_InText.split("<<<");
   for(int i = 1; i < L_open.size(); i++) {
     QStringList L_close = L_open[i].split(">>>");
-    qDebug()<<"L_close[0]="<<L_close[0];
     QStringList L_elements = L_close[0].split(":");
     TemplateLine template_line;
-    qDebug()<<"L_elements="<<L_elements;
     template_line.type = L_elements[0];
     template_line.name = L_elements[1];
     template_line.options = L_elements[2];
@@ -117,233 +111,7 @@ void FileTemplate::setOutValues(QStringList L) {
   m_OutValues = L;
 }
 
-GuiTemplateViewer::GuiTemplateViewer(QWidget *parent) : QDialog(parent) {
-
-}
-
-GuiTemplateViewer::GuiTemplateViewer(QString filename, QWidget *parent) : QDialog(parent) {
-  
-  openButton = new QPushButton("Open...");
-  saveButton = new QPushButton("Save");
-  saveAsButton = new QPushButton("Save as...");
-  
-  connect(openButton, SIGNAL(clicked()), this, SLOT(open()));
-  connect(saveButton, SIGNAL(clicked()), this, SLOT(save()));
-  connect(saveAsButton, SIGNAL(clicked()), this, SLOT(saveAs()));
-  
-  QHBoxLayout *bottomLayout = new QHBoxLayout;
-  bottomLayout->addStretch();
-  bottomLayout->addWidget(openButton);
-  bottomLayout->addWidget(saveButton);
-  bottomLayout->addWidget(saveAsButton);
-  
-  formLayout = new QFormLayout;
-  mainLayout = new QVBoxLayout;
-  
-  this->setWindowTitle("Template Viewer");
-  
-  m_file_template.open(filename);
-  m_file_template.print();
-  m_Lines = m_file_template.getLines();
-  for(int i = 0; i < m_Lines.size(); i++) {
-    if(m_Lines[i].type == "ComboBox") addComboBox(m_Lines[i]);
-    else if(m_Lines[i].type == "IntLineEdit") addIntLineEdit(m_Lines[i]);
-    else if(m_Lines[i].type == "DoubleLineEdit") addDoubleLineEdit(m_Lines[i]);
-    else if(m_Lines[i].type == "TextLineEdit") addTextLineEdit(m_Lines[i]);
-    else if(m_Lines[i].type == "CheckBox") addCheckBox(m_Lines[i]);
-    else if(m_Lines[i].type == "SpinBox") addSpinBox(m_Lines[i]);
-    else if(m_Lines[i].type == "DoubleSpinBox") addDoubleSpinBox(m_Lines[i]);
-    else qDebug()<<"Unknown type";
-  }
-  
-  mainLayout->addLayout(formLayout);
-  mainLayout->addLayout(bottomLayout);
-  this->setLayout(mainLayout);
-}
-
-GuiTemplateViewer::GuiTemplateViewer(MultipleFileTemplate multiple_file_template, QWidget *parent) : QDialog(parent) {
-  
-  openButton = new QPushButton("Open...");
-  saveButton = new QPushButton("Save");
-  saveAsButton = new QPushButton("Save as...");
-  
-  connect(openButton, SIGNAL(clicked()), this, SLOT(open()));
-  connect(saveButton, SIGNAL(clicked()), this, SLOT(save()));
-  connect(saveAsButton, SIGNAL(clicked()), this, SLOT(saveAs()));
-  
-  QHBoxLayout *bottomLayout = new QHBoxLayout;
-  bottomLayout->addStretch();
-  bottomLayout->addWidget(openButton);
-  bottomLayout->addWidget(saveButton);
-  bottomLayout->addWidget(saveAsButton);
-  
-  formLayout = new QFormLayout;
-  mainLayout = new QVBoxLayout;
-  
-  this->setWindowTitle("Template Viewer");
-  
-  m_Files = multiple_file_template;
-  
-  for(int i_fileinfo = 0; i_fileinfo<m_Files.m_FileInfos.size(); i_fileinfo++) {
-    QString filename = m_Files.m_FileInfos[i_fileinfo].filePath();
-    m_file_template.open(filename);
-    m_file_template.print();
-    m_Lines = m_file_template.getLines();
-    for(int i = 0; i < m_Lines.size(); i++) {
-      if(m_Lines[i].type == "ComboBox") addComboBox(m_Lines[i]);
-      else if(m_Lines[i].type == "IntLineEdit") addIntLineEdit(m_Lines[i]);
-      else if(m_Lines[i].type == "DoubleLineEdit") addDoubleLineEdit(m_Lines[i]);
-      else if(m_Lines[i].type == "TextLineEdit") addTextLineEdit(m_Lines[i]);
-      else if(m_Lines[i].type == "CheckBox") addCheckBox(m_Lines[i]);
-      else if(m_Lines[i].type == "SpinBox") addSpinBox(m_Lines[i]);
-      else if(m_Lines[i].type == "DoubleSpinBox") addDoubleSpinBox(m_Lines[i]);
-      else qDebug()<<"Unknown type";
-    }
-  }
-  
-  mainLayout->addLayout(formLayout);
-  mainLayout->addLayout(bottomLayout);
-  this->setLayout(mainLayout);
-}
-
-void GuiTemplateViewer::getValues() {
-  int combobox_idx = 0;
-  for(int i = 0; i < m_Lines.size(); i++) {
-    if(m_Lines[i].type == "ComboBox") {
-      m_OutValues<<readComboBox(combobox_idx);
-      combobox_idx++;
-    }
-    else qDebug()<<"Unknown type";
-  }
-}
-
-void GuiTemplateViewer::open() {
-}
-
-void GuiTemplateViewer::save() {
-  qDebug()<<"Saving...";
-  getValues();
-  m_file_template.setOutValues(m_OutValues);
-  m_file_template.saveAs("openfoam.txt");
-}
-
-void GuiTemplateViewer::saveAs() {
-}
-
-void GuiTemplateViewer::addComboBox(TemplateLine line) {
-  qDebug()<<"Adding a ComboBox...";
-  QComboBox* combobox = new QComboBox;
-  QStringList description;
-  QStringList value;
-  QStringList L_open = line.options.split("(");
-  for(int i = 1; i < L_open.size(); i++) {
-    QStringList L_close = L_open[i].split(")");
-    QStringList L_elements = L_close[0].split(",");
-    description<<L_elements[0];
-    value<<L_elements[1];
-  }
-  combobox->addItems(description);
-  formLayout->addRow(line.name, combobox);
-  m_ComboBoxVector.push_back(combobox);
-  m_ComboboxValues.push_back(value);
-}
-
-void GuiTemplateViewer::addIntLineEdit(TemplateLine line) {
-  qDebug()<<"Adding a IntLineEdit...";
-  QValidator *validator = new QIntValidator(this);
-  QLineEdit* int_lineedit = new QLineEdit;
-  int_lineedit->setValidator(validator);
-  int_lineedit->setText(line.options.trimmed());
-  formLayout->addRow(line.name, int_lineedit);
-  m_IntLineEditVector.push_back(int_lineedit);
-}
-
-void GuiTemplateViewer::addDoubleLineEdit(TemplateLine line) {
-  qDebug()<<"Adding a DoubleLineEdit...";
-  QValidator *validator = new QDoubleValidator(this);
-  QLineEdit* double_lineedit = new QLineEdit;
-  double_lineedit->setValidator(validator);
-  double_lineedit->setText(line.options.trimmed());
-  formLayout->addRow(line.name, double_lineedit);
-  m_DoubleLineEditVector.push_back(double_lineedit);
-}
-
-void GuiTemplateViewer::addTextLineEdit(TemplateLine line) {
-  qDebug()<<"Adding a TextLineEdit...";
-  QLineEdit* text_lineedit = new QLineEdit;
-  text_lineedit->setText(line.options.trimmed());
-  formLayout->addRow(line.name, text_lineedit);
-  m_TextLineEditVector.push_back(text_lineedit);
-}
-
-void GuiTemplateViewer::addCheckBox(TemplateLine line) {
-  qDebug()<<"Adding a CheckBox...";
-  QCheckBox* check_box = new QCheckBox;
-  QStringList L = line.options.split(",");
-  if(L[0].trimmed() == "checked") check_box->setCheckState(Qt::Checked);
-  else check_box->setCheckState(Qt::Unchecked);
-  QPair < QString, QString > values;
-  values.first = L[1];
-  values.first = L[2];
-  formLayout->addRow(line.name, check_box);
-  m_CheckBoxVector.push_back(check_box);
-  m_CheckBoxValues.push_back(values);
-}
-
-void GuiTemplateViewer::addSpinBox(TemplateLine line) {
-  qDebug()<<"Adding a SpinBox...";
-  QSpinBox* spin_box = new QSpinBox;
-  QStringList L = line.options.split(",");
-  int minimum = L[0].trimmed().toInt();
-  int maximum = L[1].trimmed().toInt();
-  int step = L[2].trimmed().toInt();
-  int value = L[3].trimmed().toInt();
-  spin_box->setRange( minimum, maximum );
-  spin_box->setSingleStep( step );
-  spin_box->setValue( value );
-  formLayout->addRow(line.name, spin_box);
-  m_SpinBoxVector.push_back(spin_box);
-}
-
-void GuiTemplateViewer::addDoubleSpinBox(TemplateLine line) {
-  qDebug()<<"Adding a DoubleSpinBox...";
-  QDoubleSpinBox* double_spin_box = new QDoubleSpinBox;
-  QStringList L = line.options.split(",");
-  double minimum = L[0].trimmed().toDouble();
-  double maximum = L[1].trimmed().toDouble();
-  double step = L[2].trimmed().toDouble();
-  int decimals = L[3].trimmed().toInt();
-  double value = L[4].trimmed().toDouble();
-  double_spin_box->setRange( minimum, maximum );
-  double_spin_box->setSingleStep( step );
-  double_spin_box->setDecimals( decimals );
-  double_spin_box->setValue( value );
-  formLayout->addRow(line.name, double_spin_box);
-  m_DoubleSpinBoxVector.push_back(double_spin_box);
-}
-
-QString GuiTemplateViewer::readComboBox(int idx) {
-  int i = m_ComboBoxVector[idx]->currentIndex();
-  return m_ComboboxValues[idx][i];
-}
-
-QString GuiTemplateViewer::readIntLineEdit(int idx) {}
-QString GuiTemplateViewer::readDoubleLineEdit(int idx) {}
-QString GuiTemplateViewer::readTextLineEdit(int idx) {}
-QString GuiTemplateViewer::readCheckBox(int idx) {}
-QString GuiTemplateViewer::readSpinBox(int idx) {}
-QString GuiTemplateViewer::readDoubleSpinBox(int idx) {}
-
-void GuiTemplateViewer::addFile(QString filename) {
-  m_Files.addFile(filename);
-}
-
-void MultipleFileTemplate::addFile(QString filename) {
-  QFileInfo file_info(filename);
-  m_FileInfos.push_back(file_info);
-}
-
-SuperGui::SuperGui(MultipleFileTemplate multiple_file_template, QWidget *parent) : QDialog(parent) {
+SuperGui::SuperGui(QVector <QString> files, QWidget *parent) : QDialog(parent) {
   
   this->setWindowTitle("Template Viewer");
   
@@ -357,10 +125,9 @@ SuperGui::SuperGui(MultipleFileTemplate multiple_file_template, QWidget *parent)
   connect(saveButton, SIGNAL(clicked()), this, SLOT(save()));
   connect(saveAsButton, SIGNAL(clicked()), this, SLOT(saveAs()));
   
-  m_Files = multiple_file_template;
-  for(int i_fileinfo = 0; i_fileinfo<m_Files.m_FileInfos.size(); i_fileinfo++) {
-    QString filename = m_Files.m_FileInfos[i_fileinfo].filePath();
-    SuperBox* box = new SuperBox(filename);
+  for(int i = 0; i < files.size(); i++) {
+    m_FileInfo.push_back(QFileInfo(files[i]));
+    SuperBox* box = new SuperBox(files[i]);
     mainLayout->addLayout(box);
     m_SuperBoxVector.push_back(box);
   }
@@ -375,7 +142,6 @@ SuperGui::SuperGui(MultipleFileTemplate multiple_file_template, QWidget *parent)
 
 SuperBox::SuperBox(QString filename, char *name, QWidget *parent) : QFormLayout(parent) {
   QFormLayout::setObjectName(name);
-  qDebug() << "Created: " << QFormLayout::objectName();
   m_file_template.open(filename);
   m_file_template.print();
   m_Lines = m_file_template.getLines();
@@ -568,8 +334,6 @@ QString SuperBox::readTextLineEdit(int idx) {
 }
 
 QString SuperBox::readCheckBox(int idx) {
-  qDebug()<<m_CheckBoxValues[idx].first;
-  qDebug()<<m_CheckBoxValues[idx].second;
   if(m_CheckBoxVector[idx]->checkState() == Qt::Checked) return m_CheckBoxValues[idx].first;
   else return m_CheckBoxValues[idx].second;
 }
--- src/filetemplate.h
+++ src/filetemplate.h
@@ -44,92 +44,15 @@ public:
   void setOutValues(QStringList L);
 };
 
-class MultipleFileTemplate{
-public:
-  QVector <QFileInfo> m_FileInfos;
-public:
-  void addFile(QString filename);
-};
-
-class GuiTemplateViewer : public QDialog
-{
-  Q_OBJECT
-  public:
-    // constructors
-    /**
-    * Constructor
-    * @param parent Parent QWidget
-    */
-    GuiTemplateViewer(QWidget *parent = 0);
-    
-    /**
-    * Constructor
-    * @param filename name of the template file to use
-    * @param parent Parent QWidget
-    */
-    GuiTemplateViewer(QString filename, QWidget *parent = 0);
-  
-    /**
-    * Constructor
-    * @param multiple_file_template MultipleFileTemplate object to use
-    * @param parent Parent QWidget
-    */
-    GuiTemplateViewer(MultipleFileTemplate multiple_file_template, QWidget *parent = 0);
-  
-    void addComboBox(TemplateLine line);
-    void addIntLineEdit(TemplateLine line);
-    void addDoubleLineEdit(TemplateLine line);
-    void addTextLineEdit(TemplateLine line);
-    void addCheckBox(TemplateLine line);
-    void addSpinBox(TemplateLine line);
-    void addDoubleSpinBox(TemplateLine line);
-  
-    void getValues();
-    void addFile(QString filename);
-  
-    QString readComboBox(int idx);
-    QString readIntLineEdit(int idx);
-    QString readDoubleLineEdit(int idx);
-    QString readTextLineEdit(int idx);
-    QString readCheckBox(int idx);
-    QString readSpinBox(int idx);
-    QString readDoubleSpinBox(int idx);
-  
-  private slots:
-    void open();
-    void save();
-    void saveAs();
-  
-  private:
-    QPushButton *openButton;
-    QPushButton *saveButton;
-    QPushButton *saveAsButton;
-    QFormLayout *formLayout;
-    QVBoxLayout *mainLayout;
-    QHBoxLayout *bottomLayout;
-    FileTemplate m_file_template;
-    QVector <TemplateLine> m_Lines;
-    QStringList m_OutValues;
-    
-    QVector <QComboBox*> m_ComboBoxVector;
-    QVector <QStringList> m_ComboboxValues;
-    QVector <QLineEdit*> m_IntLineEditVector;
-    QVector <QLineEdit*> m_DoubleLineEditVector;
-    QVector <QLineEdit*> m_TextLineEditVector;
-    QVector <QCheckBox*> m_CheckBoxVector;
-    QVector < QPair <QString, QString> > m_CheckBoxValues;
-    QVector <QSpinBox*> m_SpinBoxVector;
-    QVector <QDoubleSpinBox*> m_DoubleSpinBoxVector;
-  
-    MultipleFileTemplate m_Files;
-};
-
 class SuperBox : public QFormLayout
 {
   Q_OBJECT
-    private:
-    FileTemplate m_file_template;
-    QVector <TemplateLine> m_Lines;
+  
+private:
+  FileTemplate m_file_template;
+  QVector <TemplateLine> m_Lines;
+  QStringList m_OutValues;
+  
   QVector <QComboBox*> m_ComboBoxVector;
   QVector <QStringList> m_ComboboxValues;
   QVector <QLineEdit*> m_IntLineEditVector;
@@ -140,10 +63,8 @@ class SuperBox : public QFormLayout
   QVector <QSpinBox*> m_SpinBoxVector;
   QVector <QDoubleSpinBox*> m_DoubleSpinBoxVector;
   
-  QStringList m_OutValues;
-  
-    public:
-    SuperBox(QString filename, char *name=0, QWidget *parent=0);
+public:
+  SuperBox(QString filename, char *name=0, QWidget *parent=0);
   
   void addComboBox(TemplateLine line);
   void addIntLineEdit(TemplateLine line);
@@ -166,25 +87,15 @@ class SuperBox : public QFormLayout
   void open();
   void save();
   void saveAs();
-  
-  ~SuperBox()
-  {
-    qDebug() << "Deleted: " << QFormLayout::objectName();
-  }
 };
 
 class SuperGui : public QDialog
 {
   Q_OBJECT
 public:
-  // constructors
-  /**
-  * Constructor
-  * @param parent Parent QWidget
-  */
-  SuperGui(MultipleFileTemplate multiple_file_template, QWidget *parent = 0);
+  SuperGui(QVector <QString> files, QWidget *parent = 0);
 private:
-  MultipleFileTemplate m_Files;
+  QVector <QFileInfo> m_FileInfo;
   QVector <SuperBox*> m_SuperBoxVector;
 private slots:
   void open();
