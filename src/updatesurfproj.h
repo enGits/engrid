05b3c55d020eb1493673ad14d15bdbea1ac8c1c7
Oliver Gloth <ogloth@engits.com>
2009-07-04 20:09:38 +0200

    cleaning up projection

---
+++ src/updatesurfproj.h
@@ -0,0 +1,18 @@
+#ifndef UPDATESURFPROJ_H
+#define UPDATESURFPROJ_H
+
+class UpdateSurfProj;
+
+#include "operation.h"
+
+class UpdateSurfProj : public Operation
+{
+
+public:
+
+  UpdateSurfProj();
+  virtual void operate();
+
+};
+
+#endif // UPDATESURFPROJ_H
