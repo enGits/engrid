5177f4d6ce51ceea9eb0ab70e127d84295f7567d
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-07-20 16:41:05 +0200

    removed -j2 for nightly builds

--- src/scripts/rebuild.sh
+++ src/scripts/rebuild.sh
@@ -27,16 +27,18 @@ echo LD_LIBRARY_PATH = $LD_LIBRARY_PATH
 echo "Building netgen"
 ./build-nglib.sh
 
+MAKEOPTIONS=""
+
 echo "Building engrid.pro release version"
-qmake && make distclean && qmake engrid.pro && make -j2 || exit 1
+qmake && make distclean && qmake engrid.pro && make $MAKEOPTIONS release || exit 1
 
 echo "Building engrid.pro.cgns release version"
-qmake && make distclean && qmake engrid.pro.cgns && make -j2 || exit 1
+qmake && make distclean && qmake engrid.pro.cgns && make $MAKEOPTIONS release || exit 1
 
 echo "Building engrid.pro debug version"
-qmake && make distclean && qmake engrid.pro && make -j2 debug || exit 1
+qmake && make distclean && qmake engrid.pro && make $MAKEOPTIONS debug || exit 1
 
 echo "Building engrid.pro.cgns debug version"
-qmake && make distclean && qmake engrid.pro.cgns && make -j2 debug || exit 1
+qmake && make distclean && qmake engrid.pro.cgns && make $MAKEOPTIONS debug || exit 1
 
 echo "SUCCESS: Everything compiles."
