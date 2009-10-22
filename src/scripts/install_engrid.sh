5cfe1fe89ae2ba4d58e8ab1a8268a39657dd7eb5
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-10-22 13:25:21 +0200

    created separate config file for script

--- src/scripts/install_engrid.sh
+++ src/scripts/install_engrid.sh
@@ -3,33 +3,25 @@
 #for debugging
 set -eux
 
-#variables to set
-ARCHIVE_QT="qt-x11-opensource-src-4.5.2.tar.gz"
-URL_QT="http://get.qt.nokia.com/qt/source/$ARCHIVE_QT"
+source "${0%/*}/engrid_installer_updater.cfg"
 
-ARCHIVE_VTK="vtk-5.4.2.tar.gz"
-URL_VTK="http://www.vtk.org/files/release/5.4/$ARCHIVE_VTK"
+create_bash_engrid()
+{
+  echo "Create bash_engrid"
 
-ARCHIVE_CGNS="cgnslib_2.5-4.tar.gz"
-URL_CGNS="http://prdownloads.sourceforge.net/cgns/$ARCHIVE_CGNS"
-
-#HTTP
-# URL_ENGRID="http://engits.com/git/engrid.git"
-#SSH
-URL_ENGRID="ssh://swordfish/srv/www/htdocs/git/engrid.git"
-
-PREFIX="$(readlink -f .)/usr/engrid_libs"
-mkdir -p $PREFIX
-
-VTKPREFIX=$PREFIX/VTK
-VTKVERSION="5.4"
-QTPREFIX=$PREFIX/QT
-CGNSPREFIX=$PREFIX/CGNS
-# VTK_WRAP_JAVA
-# VTK_WRAP_PYTHON
-# VTK_WRAP_TCL
-
-ENV_SETUP="engrid_environment.sh"
+  echo "#!/usr/bin/env bash" > $ENV_SETUP
+  echo "export VTKINCDIR=$VTKPREFIX/include/vtk-$VTKVERSION" >> $ENV_SETUP
+  echo "export VTKLIBDIR=$VTKPREFIX/lib/vtk-$VTKVERSION" >> $ENV_SETUP
+  echo "export LD_LIBRARY_PATH=$VTKLIBDIR:\$LD_LIBRARY_PATH" >> $ENV_SETUP
+  
+  echo "export CGNSINCDIR=/opt/shared/cgns/include" >> $ENV_SETUP
+  echo "export CGNSLIBDIR=/opt/shared/cgns/lib" >> $ENV_SETUP
+  echo "export LD_LIBRARY_PATH=$CGNSLIBDIR:\$LD_LIBRARY_PATH" >> $ENV_SETUP
+  
+  echo "export PATH=$QTPREFIX/bin:\$PATH" >> $ENV_SETUP
+  echo "export QTDIR=$QTPREFIX" >> $ENV_SETUP
+  echo "export LD_LIBRARY_PATH=$QTPREFIX/lib:\$LD_LIBRARY_PATH" >> $ENV_SETUP
+}
 
 install_QT()
 {
@@ -76,39 +68,32 @@ build_engrid()
   echo "Build netgen"
   ./scripts/build-nglib.sh
   echo "Build enGrid"
-  qmake && make distclean && qmake && make release
+  qmake && make release
   cd -
 }
 
 update_netgen()
 {
+  cd ./engrid/src
   echo "Update netgen"
   ./scripts/build-nglib.sh
+  cd -
 }
 
 update_engrid()
 {
+  cd ./engrid/src
   echo "Update enGrid"
   git pull
-  qmake && make distclean && qmake && make release
+  qmake && make release
+  cd -
 }
 
-create_bash_engrid()
+rebuild_engrid()
 {
-  echo "Create bash_engrid"
-
-  echo "#!/usr/bin/env bash" > $ENV_SETUP
-  echo "export VTKINCDIR=$VTKPREFIX/include/vtk-$VTKVERSION" >> $ENV_SETUP
-  echo "export VTKLIBDIR=$VTKPREFIX/lib/vtk-$VTKVERSION" >> $ENV_SETUP
-  echo "export LD_LIBRARY_PATH=$VTKLIBDIR:\$LD_LIBRARY_PATH" >> $ENV_SETUP
-  
-  echo "export CGNSINCDIR=/opt/shared/cgns/include" >> $ENV_SETUP
-  echo "export CGNSLIBDIR=/opt/shared/cgns/lib" >> $ENV_SETUP
-  echo "export LD_LIBRARY_PATH=$CGNSLIBDIR:\$LD_LIBRARY_PATH" >> $ENV_SETUP
-  
-  echo "export PATH=$QTPREFIX/bin:\$PATH" >> $ENV_SETUP
-  echo "export QTDIR=$QTPREFIX" >> $ENV_SETUP
-  echo "export LD_LIBRARY_PATH=$QTPREFIX/lib:\$LD_LIBRARY_PATH" >> $ENV_SETUP
+  cd ./engrid/src
+  qmake && make distclean && qmake && make release
+  cd -
 }
 
 create_bash_engrid
@@ -116,10 +101,10 @@ source $ENV_SETUP
 # install_QT
 # install_VTK
 # install_CGNS
-build_engrid
-cd engrid/src
+# build_engrid
 update_netgen
 update_engrid
-cd -
+# rebuild_engrid
 
+echo "SUCCESS"
 exit 0
