781e803a55f5c3ba4196e1b4047621ad09ad8a2b
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-10-23 10:53:14 +0200

    documented engrid_installer_updater.sh + engrid_installer_updater.cfg

--- src/scripts/engrid_installer_updater.sh
+++ src/scripts/engrid_installer_updater.sh
@@ -1,4 +1,74 @@
 #!/usr/bin/env bash
+#
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# +                                                                      +
+# + This file is part of enGrid.                                         +
+# +                                                                      +
+# + Copyright 2008,2009 Oliver Gloth                                     +
+# +                                                                      +
+# + enGrid is free software: you can redistribute it and/or modify       +
+# + it under the terms of the GNU General Public License as published by +
+# + the Free Software Foundation, either version 3 of the License, or    +
+# + (at your option) any later version.                                  +
+# +                                                                      +
+# + enGrid is distributed in the hope that it will be useful,            +
+# + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
+# + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
+# + GNU General Public License for more details.                         +
+# +                                                                      +
+# + You should have received a copy of the GNU General Public License    +
+# + along with enGrid. If not, see <http:#www.gnu.org/licenses/>.        +
+# +                                                                      +
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+# DESCRIPTION:
+# This script allows:
+# -downloading, building and installing the dependencies for engrid
+# -downloading, building and installing engrid
+# -updating netgen
+# -updating engrid
+# -rebuilding engrid
+# -generating an environment setup script that can then be sourced by ~/.bashrc
+# -generating a startup script that sets up the environment and then starts engrid
+#
+# USAGE:
+# First of all change the configuration file engrid_installer_updater.cfg according to your needs.
+# Then you can run this script and choose the actions you wish to execute. Multiple actions can be run at once. They will be run in the order of the checklist.
+# Note: Altough it should be enough to run create_bash_engrid once, it's recommended to run it every time to make sure the other actions use the correct environment.
+#
+# EXAMPLES
+# Engrid installation:
+# [X] create_bash_engrid
+# [X] install_QT
+# [X] install_VTK
+# [X] install_CGNS
+# [X] build_engrid
+# [ ] update_netgen
+# [ ] update_engrid
+# [ ] rebuild_engrid
+# [X] create_start_engrid
+#
+# Engrid update:
+# [X] create_bash_engrid
+# [ ] install_QT
+# [ ] install_VTK
+# [ ] install_CGNS
+# [ ] build_engrid
+# [X] update_netgen
+# [X] update_engrid
+# [ ] rebuild_engrid
+# [ ] create_start_engrid
+#
+# Engrid rebuild:
+# [X] create_bash_engrid
+# [ ] install_QT
+# [ ] install_VTK
+# [ ] install_CGNS
+# [ ] build_engrid
+# [ ] update_netgen
+# [ ] update_engrid
+# [X] rebuild_engrid
+# [ ] create_start_engrid
 
 #for debugging
 set -eux
