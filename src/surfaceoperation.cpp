3d2b6ab17bbe9846ed23a6af697aa8f7658cc7d8
Oliver Gloth <ogloth@engits.com>
2009-11-06 10:37:09 +0100

    ficed rad/deg problem

--- src/surfaceoperation.cpp
+++ src/surfaceoperation.cpp
@@ -42,6 +42,7 @@ SurfaceOperation::SurfaceOperation()
   //m_AllowFeatureEdgeVertices = 1;//0 by default in VTK, but we need 1 to avoid the "potatoe effect" ^^
   getSet("surface meshing", "edge angle to determine fixed vertices", 180, m_EdgeAngle);
   getSet("surface meshing", "feature angle", 180, m_FeatureAngle);
+  m_FeatureAngle = GeometryTools::deg2rad(m_FeatureAngle);
   m_EdgeAngle = GeometryTools::deg2rad(m_EdgeAngle);
   setEdgeAngle(m_EdgeAngle);
   m_BoundarySmoothing = 1;
