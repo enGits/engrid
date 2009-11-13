d28c4816dbed3458b8013da9efb637cc9853b091
Oliver Gloth <ogloth@engits.com>
2009-11-13 14:46:20 +0100

    added new classes for edge length definitions

---
+++ src/edgelengthsourcemanager.cpp
@@ -0,0 +1,142 @@
+//
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +                                                                      +
+// + This file is part of enGrid.                                         +
+// +                                                                      +
+// + Copyright 2008,2009 Oliver Gloth                                     +
+// +                                                                      +
+// + enGrid is free software: you can redistribute it and/or modify       +
+// + it under the terms of the GNU General Public License as published by +
+// + the Free Software Foundation, either version 3 of the License, or    +
+// + (at your option) any later version.                                  +
+// +                                                                      +
+// + enGrid is distributed in the hope that it will be useful,            +
+// + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
+// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
+// + GNU General Public License for more details.                         +
+// +                                                                      +
+// + You should have received a copy of the GNU General Public License    +
+// + along with enGrid. If not, see <http://www.gnu.org/licenses/>.       +
+// +                                                                      +
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//
+
+#include "edgelengthsourcemanager.h"
+
+#include "guiedgelengthsourcesphere.h"
+#include "guimainwindow.h"
+
+EdgeLengthSourceManager::EdgeLengthSourceManager()
+{
+  m_Sources.clear();
+  m_Samples.clear();
+  m_Samples.push_back(new GuiEdgeLengthSourceSphere);
+  m_ListWidget = NULL;
+}
+
+EdgeLengthSourceManager::~EdgeLengthSourceManager()
+{
+  foreach (EdgeLengthSource* source, m_Sources) {
+    //delete source;
+  }
+  foreach (EdgeLengthSource* sample, m_Samples) {
+    //delete sample;
+  }
+}
+
+void EdgeLengthSourceManager::populateListWidget()
+{
+  if (m_ListWidget) {
+    m_ListWidget->clear();
+    foreach (EdgeLengthSource *source, m_Sources) {
+      m_ListWidget->addItem(source->name());
+    }
+  }
+}
+
+void EdgeLengthSourceManager::read()
+{
+  foreach (EdgeLengthSource* source, m_Sources) {
+    //delete source;
+  }
+  m_Sources.clear();
+  QString xml_text = GuiMainWindow::pointer()->getXmlSection("engrid/sources");
+  QStringList lines = xml_text.split("\n");
+  foreach (QString line, lines) {
+    foreach (EdgeLengthSource* sample, m_Samples) {
+      if (sample->read(line.trimmed())) {
+        if (dynamic_cast<GuiEdgeLengthSourceSphere*>(sample)) {
+          GuiEdgeLengthSourceSphere *S = new GuiEdgeLengthSourceSphere;
+          S->read(line.trimmed());
+          m_Sources.push_back(S);
+          break;
+        }
+      }
+    }
+  }
+}
+
+void EdgeLengthSourceManager::write()
+{
+  QString xml_text = "";
+  foreach (EdgeLengthSource* source, m_Sources) {
+    xml_text += source->write();
+    xml_text += "\n";
+  }
+  GuiMainWindow::pointer()->setXmlSection("engrid/sources", xml_text);
+}
+
+double EdgeLengthSourceManager::minEdgeLength(vec3_t x)
+{
+  double L_min = 1e99;
+  foreach (EdgeLengthSource* source, m_Sources) {
+    double L = source->edgeLength(x);
+    if (L > 0) {
+      L_min = min(L, L_min);
+    }
+  }
+  return L_min;
+}
+
+void EdgeLengthSourceManager::edit()
+{
+  if (m_ListWidget->currentItem()) {
+    QString selected_name = m_ListWidget->currentItem()->text();
+    foreach (EdgeLengthSource* source, m_Sources) {
+      if (source->name() == selected_name) {
+        source->config();
+        break;
+      }
+    }
+    populateListWidget();
+  }
+}
+
+void EdgeLengthSourceManager::remove()
+{
+  if (m_ListWidget->currentItem()) {
+    QList<EdgeLengthSource*> new_sources;
+    QString selected_name = m_ListWidget->currentItem()->text();
+    foreach (EdgeLengthSource* source, m_Sources) {
+      if (source->name() == selected_name) {
+        delete source;
+      } else {
+        new_sources.push_back(source);
+      }
+    }
+    m_Sources = new_sources;
+    populateListWidget();
+  }
+}
+
+void EdgeLengthSourceManager::addSphere()
+{
+  QString name = "sphere" + timeStamp();
+  GuiEdgeLengthSourceSphere *S = new GuiEdgeLengthSourceSphere;
+  S->setName(name);
+  m_Sources.append(S);
+  populateListWidget();
+}
+
+
+
---
+++ src/edgelengthsourcemanager.h
@@ -0,0 +1,65 @@
+//
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +                                                                      +
+// + This file is part of enGrid.                                         +
+// +                                                                      +
+// + Copyright 2008,2009 Oliver Gloth                                     +
+// +                                                                      +
+// + enGrid is free software: you can redistribute it and/or modify       +
+// + it under the terms of the GNU General Public License as published by +
+// + the Free Software Foundation, either version 3 of the License, or    +
+// + (at your option) any later version.                                  +
+// +                                                                      +
+// + enGrid is distributed in the hope that it will be useful,            +
+// + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
+// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
+// + GNU General Public License for more details.                         +
+// +                                                                      +
+// + You should have received a copy of the GNU General Public License    +
+// + along with enGrid. If not, see <http://www.gnu.org/licenses/>.       +
+// +                                                                      +
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//
+
+#ifndef EDGELENGTHSOURCEMANAGER_H
+#define EDGELENGTHSOURCEMANAGER_H
+
+#include <QListWidget>
+#include <QDateTime>
+
+#include "egvtkobject.h"
+#include "edgelengthsource.h"
+
+class EdgeLengthSourceManager : public EgVtkObject
+{
+
+private: // attributes
+
+  QList<EdgeLengthSource*> m_Sources;
+  QList<EdgeLengthSource*> m_Samples;
+  QListWidget*             m_ListWidget;
+
+
+private: // methods
+
+  QString timeStamp() { return QDateTime::currentDateTime().toString("_yyyyMMddhhmmss"); }
+
+
+public:
+
+  EdgeLengthSourceManager();
+  virtual ~EdgeLengthSourceManager();
+
+  void   setListWidget(QListWidget *list_widget) { m_ListWidget = list_widget; }
+  double minEdgeLength(vec3_t x);
+  void   populateListWidget();
+
+  void read();
+  void write();
+  void edit();
+  void remove();
+  void addSphere();
+
+};
+
+#endif // EDGELENGTHSOURCEMANAGER_H
