c9ef30f46a40bfcdda4f269fd96c19945362314f
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-11-17 13:09:53 +0100

    merged in changes to geometrytools from projection_branch

--- src/geometrytools.h
+++ src/geometrytools.h
@@ -62,6 +62,22 @@ double intersection(vec3_t x_straight, vec3_t v_straight,
 double intersection(vec3_t x_straight, vec3_t v_straight, 
                     vec3_t x_plane, vec3_t u_plane, vec3_t v_plane);
 
+/**
+ * Calculates the intersection of a segment [x1,x2] with a triangle (a,b,c)
+ * Note: (xi,ri) will always be set to the intersection of the line (x1,x2) with the plane (a,b,c) even if the segment does not intersect the triangle!
+ * @param a Input: Triangle point 1
+ * @param b Input: Triangle point 2
+ * @param c Input: Triangle point 3
+ * @param x1 Input: Segment point 1
+ * @param x2 Input: Segment point 2
+ * @param xi Output: 3D Global coordinates of the intersection point
+ * @param ri Output: 3D local triangle coordinates of the intersection point
+ * @param tol Input: Relative tolerance: There can only be an intersection if:
+ * 0-tol*(x1-x2).abs()<=k<=1+tol*(x1-x2).abs()
+ * 0-tol<=ri[0]<=1+tol
+ * 0-tol<=ri[1]<=1+tol
+ * @return true if an intersection point was found, else false.
+*/
 bool intersectEdgeAndTriangle(const vec3_t& a, const vec3_t& b, const vec3_t& c,
                               const vec3_t& x1, const vec3_t& x2, vec3_t& xi, vec3_t& ri, double tol = 1e-4);
 
@@ -168,6 +184,12 @@ double distance2(vtkUnstructuredGrid *grid, vtkIdType id_node1, vtkIdType id_nod
 double areaOfCircumscribedCircle(vtkUnstructuredGrid *grid, vtkIdType id_cell);
 
 vec3_t getBarycentricCoordinates(double x, double y);
+
+vec3_t intersectionOnPlane(vec3_t v, vec3_t A, vec3_t nA, vec3_t B, vec3_t nB);
+
+vec2_t projectVectorOnPlane(vec3_t V,vec3_t i,vec3_t j);
+
+vec3_t projectPointOnEdge(const vec3_t& M,const vec3_t& A, const vec3_t& u);
 
 };
 
