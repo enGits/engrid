72d0cda5538f707d7a35dab24ad972238e726a69
Mike Taverne <mtaverne@bluemarlin.engits.com>
2009-11-17 11:50:16 +0100

    grid->m_grid changes from projection_branch

--- src/engrid.h
+++ src/engrid.h
@@ -197,7 +197,7 @@ if (GRID->GetPointData()->GetScalars(NAME)) { \
 OPER *oper = new OPER(); \
 (*oper)(); \
 oper->del(); \
-if(grid->GetNumberOfPoints()) updateBoundaryCodes(false); \
+if(m_grid->GetNumberOfPoints()) updateBoundaryCodes(false); \
 updateActors(); \
 
 #define EG_STDSLOT(OPER) \
@@ -211,7 +211,7 @@ updateActors(); \
 OPER *oper = new OPER(); \
 (*oper)(); \
 oper->del(); \
-if(grid->GetNumberOfPoints()) updateBoundaryCodes(true); \
+if(m_grid->GetNumberOfPoints()) updateBoundaryCodes(true); \
 updateActors(); \
 updateStatusBar(); \
 zoomAll();
