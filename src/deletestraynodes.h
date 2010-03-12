6a13ec419d104b25aaedcf00108388c2882b15ec
Oliver Gloth <ogloth@engits.com>
2010-03-12 13:25:31 +0100

    added deletion for 'stray' nodes

---
+++ src/deletestraynodes.h
@@ -0,0 +1,45 @@
+//
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +                                                                      +
+// + This file is part of enGrid.                                         +
+// +                                                                      +
+// + Copyright 2008-2010 Oliver Gloth                                     +
+// +                                                                      +
+// + enGrid is free software: you can redistribute it and/or modify       +
+// + it under the terms of the GNU General Public License as published by +
+// + the Free Software Foundation, either version 3 of the License, or    +
+// + (at your option) any later version.                                  +
+// +                                                                      +
+// + enGrid is distributed in the hope that it will be useful,            +
+// + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
+// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
+// + GNU General Public License for more details.                         +
+// +                                                                      +
+// + You should have received a copy of the GNU General Public License    +
+// + along with enGrid. If not, see <http://www.gnu.org/licenses/>.       +
+// +                                                                      +
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//
+#ifndef DELETESTRAYNODES_H
+#define DELETESTRAYNODES_H
+
+#include "operation.h"
+
+/**
+ * Delete nodes that are not part of any cell.
+ */
+class DeleteStrayNodes : public Operation
+{
+
+public: // methods
+
+  DeleteStrayNodes();
+
+
+protected: // methods
+
+  virtual void operate();
+
+};
+
+#endif // DELETESTRAYNODES_H
