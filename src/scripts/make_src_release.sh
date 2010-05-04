0dd868767638d1f380e7514a1e74799df57ef737
Oliver Gloth <ogloth@engits.com>
2010-05-04 18:57:46 +0200

    added helper scripts

---
+++ src/scripts/make_src_release.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# +                                                                      +
+# + This file is part of enGrid.                                         +
+# +                                                                      +
+# + Copyright 2008-2010 enGits GmbH                                     +
+# +                                                                      +
+# + enGrid is free software: you can redistribute it and/or modify       +
+# + it under the terms of the GNU General Public License as published by +
+# + the Free Software Foundation, either version 3 of the License, or    +
+# + (at your option) any later version.                                  +
+# +                                                                      +
+# + enGrid is distributed in the hope that it will be useful,            +
+# + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
+# + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
+# + GNU General Public License for more details.                         +
+# +                                                                      +
+# + You should have received a copy of the GNU General Public License    +
+# + along with enGrid. If not, see <http://www.gnu.org/licenses/>.       +
+# +                                                                      +
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 
+mkdir git.tmp
+cd git.tmp
+git clone ssh://engits.eu/git/engrid.git
+cd engrid
+git checkout $1
+rm -rf .git
+cp -R ../../netgen_svn/netgen-mesher src/netgen_svn/
+rm -rf src/netgen_svn/netgen-mesher/.svn
+cd ..
+tar cvzf ../engrid-src-$1.tar.gz engrid
+cd ..
+rm -rf git.tmp
