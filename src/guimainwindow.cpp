7178ba9182552e24e5499eadde5f4c1adf802b56
Oliver Gloth <ogloth@engits.com>
2010-05-06 12:10:49 +0200

    compatibility with Qt4.4

--- src/guimainwindow.cpp
+++ src/guimainwindow.cpp
@@ -1985,9 +1985,15 @@ void GuiMainWindow::callInsertNewCell()
 {
   bool ok1,ok2,ok3,ok4;
   vtkIdType pts[3];
+#if QT_VERSION < 0x040500
+  pts[0] = QInputDialog::getInteger(this, tr("id_node1"),tr("id_node1:"), 0, 0, m_Grid->GetNumberOfPoints(), 1, &ok1);
+  pts[1] = QInputDialog::getInteger(this, tr("id_node2"),tr("id_node2:"), 0, 0, m_Grid->GetNumberOfPoints(), 1, &ok2);
+  pts[2] = QInputDialog::getInteger(this, tr("id_node3"),tr("id_node3:"), 0, 0, m_Grid->GetNumberOfPoints(), 1, &ok3);
+#else
   pts[0] = QInputDialog::getInt(this, tr("id_node1"),tr("id_node1:"), 0, 0, m_Grid->GetNumberOfPoints(), 1, &ok1);
   pts[1] = QInputDialog::getInt(this, tr("id_node2"),tr("id_node2:"), 0, 0, m_Grid->GetNumberOfPoints(), 1, &ok2);
   pts[2] = QInputDialog::getInt(this, tr("id_node3"),tr("id_node3:"), 0, 0, m_Grid->GetNumberOfPoints(), 1, &ok3);
+#endif
   vtkIdType id_cell = QInputDialog::getInt(this, tr("copy cell data from id_cell"),tr("copy cell data from id_cell:"), 0, 0, m_Grid->GetNumberOfCells(), 1, &ok4);
   if (ok1 && ok2 && ok3 && ok4) {
     EG_VTKSP( vtkUnstructuredGrid, new_grid );
