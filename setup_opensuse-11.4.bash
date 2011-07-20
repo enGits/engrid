ade85ff95d474c561a687b269a547d4e1cfef7c0
Oliver Gloth <ogloth@engits.com>
2011-07-20 15:26:28 +0200

    improved bindary setup

--- setup_opensuse-11.4.bash
+++ setup_opensuse-11.4.bash
@@ -3,6 +3,7 @@ olddir=`pwd`
 cd `dirname $0`
 sudo zypper addrepo http://download.opensuse.org/repositories/science/openSUSE_11.4/ science
 sudo zypper install vtk-qt
-echo "You can start enGrid by typing: `pwd`/run.bash (as non-root user)"
+echo "`pwd`/run.bash" >> engrid
+sudo cp engrid /usr/bin
 cd $olddir
-
+echo "You can start enGrid by typing 'engrid' as non-root user."
