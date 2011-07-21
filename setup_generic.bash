529ebe25751aabed21109a9f909e3ed2a511bb09
Oliver Gloth <ogloth@engits.com>
2011-07-21 20:43:41 +0200

    generic script to install enGrid after building it

---
+++ setup_generic.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+olddir=`pwd`
+cd `dirname $0`
+cp engrid iengrid
+echo "`pwd`/run.bash" >> iengrid
+sudo cp iengrid /usr/bin/engrid
+cp engrid.desktop iengrid.desktop
+echo "Icon=`pwd`/src/libengrid/resources/icons/G.png" >> iengrid.desktop
+sudo cp iengrid.desktop /usr/share/applications/engrid.desktop
+cd $olddir
+echo "You can start enGrid by typing 'engrid' from the command line (as non-root user) or with the help of the desktop menus."
