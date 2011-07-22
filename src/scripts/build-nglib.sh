28f17672485b21a9d5a96500f3041bf572eafd6e
Oliver Gloth <ogloth@engits.com>
2011-07-22 18:19:58 +0200

    added -f to rm command to avoid prompting

--- src/scripts/build-nglib.sh
+++ src/scripts/build-nglib.sh
@@ -27,7 +27,7 @@
 cd netgen_svn
 wget http://engits.eu/files/netgen-4.9.13.zip
 unzip netgen-4.9.13.zip
-rm netgen-4.9.13.zip
+rm -f netgen-4.9.13.zip
 rm -rf netgen-mesher
 mkdir netgen-mesher
 mv netgen-4.9.13 netgen-mesher/netgen
