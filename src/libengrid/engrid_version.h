18bfad7e9e12a6c3ea697855be1b94bbdf70b8a3
bmss <bruno.santos@bluecape.com.pt>
2011-07-26 14:23:18 +0100

    Improved version description.

--- src/libengrid/engrid_version.h
+++ src/libengrid/engrid_version.h
@@ -22,8 +22,9 @@
 // 
 #ifndef ENGRID_VERSION
 #define ENGRID_VERSION "1.3alpha"
-#define ENGRIDVERSIONFULLVER 1.3.0.0
-#define COMPANYNAME "enGits GmbH"
-#define ENGRIDDESCRIPTION "enGrid"
-#define ENGRIDCOPYRIGHT "GNU Public License (GPL). Developed by enGits GmbH http://www.engits.eu"
+#define ENGRID_VERSION_FULLVER 1,3,0,0
+#define ENGRID_COMPANY_NAME "enGits GmbH"
+#define ENGRID_DESCRIPTION "enGrid is an open-source mesh generation software with CFD applications in mind."
+#define ENGRID_COPYRIGHT "GNU Public License (GPL). Developed by enGits GmbH - http://www.engits.eu"
+#define ENGRID_NAME "enGrid"
 #endif
