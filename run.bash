0ba9690d8e12bcbcb86c031636383ab1b46183cc
Bruno Santos <bruno.santos@bluecape.com.pt>
2011-12-23 17:09:58 +0000

    Forgot that nglib now is a dynamic library.

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 dir=`dirname $0`
-export LD_LIBRARY_PATH=$dir/src/libengrid:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$dir/src/libengrid:$dir/src/netgen_svn:$LD_LIBRARY_PATH
 $dir/src/engrid                                                                                                                                                 
