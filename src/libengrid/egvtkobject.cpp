fe9948076a1fa97ef585ede87fbeddea9e412844
Oliver Gloth <ogloth@engits.com>
2012-03-22 15:17:44 +0100

    removed unnecessary fields

--- src/libengrid/egvtkobject.cpp
+++ src/libengrid/egvtkobject.cpp
@@ -719,7 +719,7 @@ void EgVtkObject::copyNodeData
   EGVTKOBJECT_COPYNODEDATA("node_index", vtkLongArray_t);
   EGVTKOBJECT_COPYNODEDATA("node_specified_density", vtkIntArray);
   EGVTKOBJECT_COPYNODEDATA("node_meshdensity_desired",  vtkDoubleArray);
-  EGVTKOBJECT_COPYNODEDATA("node_meshdensity_current",  vtkDoubleArray);
+  //EGVTKOBJECT_COPYNODEDATA("node_meshdensity_current",  vtkDoubleArray);
   EGVTKOBJECT_COPYNODEDATA("node_type",  vtkCharArray);
   EGVTKOBJECT_COPYNODEDATA("node_pindex", vtkLongArray_t);
 }
@@ -772,7 +772,7 @@ void EgVtkObject::createBasicCellFields(vtkUnstructuredGrid *grid, vtkIdType Nce
   EGVTKOBJECT_CREATECELLFIELD("cell_orgdir", vtkIntArray, overwrite); // original orientation
   EGVTKOBJECT_CREATECELLFIELD("cell_curdir", vtkIntArray, overwrite); // current orientation
   EGVTKOBJECT_CREATECELLFIELD("cell_voldir", vtkIntArray, overwrite); // volume orientation -- only valid for a single (i.e. the current) volume
-  EGVTKOBJECT_CREATECELLFIELD("cell_VA",     vtkDoubleArray, overwrite);
+  //EGVTKOBJECT_CREATECELLFIELD("cell_VA",     vtkDoubleArray, overwrite);
 }
 
 void EgVtkObject::createBasicNodeFields(vtkUnstructuredGrid *grid, vtkIdType Nnodes, bool overwrite)
@@ -782,7 +782,7 @@ void EgVtkObject::createBasicNodeFields(vtkUnstructuredGrid *grid, vtkIdType Nno
   EGVTKOBJECT_CREATENODEFIELD("node_index",                vtkLongArray_t, overwrite);
   EGVTKOBJECT_CREATENODEFIELD("node_specified_density",    vtkIntArray,    overwrite); //density index from table
   EGVTKOBJECT_CREATENODEFIELD("node_meshdensity_desired",  vtkDoubleArray, overwrite); //what we want
-  EGVTKOBJECT_CREATENODEFIELD("node_meshdensity_current",  vtkDoubleArray, overwrite); //what we have
+  //EGVTKOBJECT_CREATENODEFIELD("node_meshdensity_current",  vtkDoubleArray, overwrite); //what we have
   EGVTKOBJECT_CREATENODEFIELD("node_type",                 vtkCharArray,   overwrite); //node type
   EGVTKOBJECT_CREATENODEFIELD("node_pindex",               vtkLongArray_t, overwrite);
 }
@@ -1170,10 +1170,10 @@ void EgVtkObject::addVtkTypeInfo(vtkUnstructuredGrid* a_grid)
   EG_VTKSP(vtkIntArray, vtk_type);
   vtk_type->SetName("vtk_type");
   vtk_type->SetNumberOfValues(a_grid->GetNumberOfCells());
-  EG_VTKDCC(vtkDoubleArray, cell_VA, a_grid, "cell_VA");
+  //EG_VTKDCC(vtkDoubleArray, cell_VA, a_grid, "cell_VA");
   for (vtkIdType cellId = 0; cellId < a_grid->GetNumberOfCells(); ++cellId) {
     vtk_type->SetValue(cellId, a_grid->GetCellType(cellId));
-    cell_VA->SetValue(cellId, GeometryTools::cellVA(a_grid, cellId, true));
+    //cell_VA->SetValue(cellId, GeometryTools::cellVA(a_grid, cellId, true));
   }
   a_grid->GetCellData()->AddArray(vtk_type);
 }
