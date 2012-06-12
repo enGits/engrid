581e21454c28e0d90397b25b31a3d09b00174da1
Oliver Gloth <ogloth@engits.com>
2012-06-12 17:32:05 +0200

    added Dolfyn export

---
+++ src/libengrid/dolfynwriter.h
@@ -0,0 +1,53 @@
+//
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +                                                                      +
+// + This file is part of enGrid.                                         +
+// +                                                                      +
+// + Copyright 2008-2012 enGits GmbH                                     +
+// +                                                                      +
+// + enGrid is free software: you can redistribute it and/or modify       +
+// + it under the terms of the GNU General Public License as published by +
+// + the Free Software Foundation, either version 3 of the License, or    +
+// + (at your option) any later version.                                  +
+// +                                                                      +
+// + enGrid is distributed in the hope that it will be useful,            +
+// + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
+// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
+// + GNU General Public License for more details.                         +
+// +                                                                      +
+// + You should have received a copy of the GNU General Public License    +
+// + along with enGrid. If not, see <http://www.gnu.org/licenses/>.       +
+// +                                                                      +
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//
+#ifndef DOLFYNWRITER_H
+#define DOLFYNWRITER_H
+
+#include "iooperation.h"
+
+class DolfynWriter : public IOOperation
+{
+
+private: // attributes
+
+  QFile* m_VrtFile;
+  QFile* m_CelFile;
+  QFile* m_BndFile;
+
+private: // methods
+  
+  void writeVertices();
+  void writeElements();
+  void writeBoundaries();
+
+protected: // methods
+
+  virtual void operate();
+
+public:
+
+  DolfynWriter();
+
+};
+
+#endif // DOLFYNWRITER_H
