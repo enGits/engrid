728bb8be0aea8d1dfcfd06cd816fc3953e258610
wyldckat <wyldckat@gmail.com>
2012-09-21 23:28:52 +0100

    openSUSE 12.2 already uses 5.10 vs 5.8 on 12.1.

--- src/scripts/setup_pathes.bash
+++ src/scripts/setup_pathes.bash
@@ -32,8 +32,10 @@ help ()
   echo "                ubuntu-10.10"
   echo "                opensuse32"
   echo "                opensuse64"
-  echo "                opensuse32-12"
-  echo "                opensuse64-12"
+  echo "                opensuse32-12.1"
+  echo "                opensuse64-12.1"
+  echo "                opensuse32-12.2"
+  echo "                opensuse64-12.2"
   echo "                fedora32"
   echo "                fedora64"
 }
@@ -68,13 +70,21 @@ else
   then
     export VTKINCDIR=/usr/include/vtk-5.8
     export VTKLIBDIR=/usr/lib64
-  elif [ $1 = 'opensuse32-12' ]
+  elif [ $1 = 'opensuse32-12.1' ]
   then
     export VTKINCDIR=/usr/include/vtk-5.8
     export VTKLIBDIR=/usr/lib
-  elif [ $1 = 'opensuse64-12' ]
+  elif [ $1 = 'opensuse64-12.1' ]
   then
     export VTKINCDIR=/usr/include/vtk-5.8
+    export VTKLIBDIR=/usr/lib64
+  elif [ $1 = 'opensuse32-12.2' ]
+  then
+    export VTKINCDIR=/usr/include/vtk-5.10
+    export VTKLIBDIR=/usr/lib
+  elif [ $1 = 'opensuse64-12.2' ]
+  then
+    export VTKINCDIR=/usr/include/vtk-5.10
     export VTKLIBDIR=/usr/lib64
   elif [ $1 = 'fedora32' ]
   then
