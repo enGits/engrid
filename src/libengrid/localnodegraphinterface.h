0630c87337495458f58e9dc6d5da965dc7c021a9
Oliver Gloth <ogloth@engits.com>
2012-09-24 09:15:19 +0200

    graph interface for nodes of a mesh

---
+++ src/libengrid/localnodegraphinterface.h
@@ -0,0 +1,48 @@
+//
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +                                                                      +
+// + This file is part of enGrid.                                         +
+// +                                                                      +
+// + Copyright 2008-2012 enGits GmbH                                      +
+// +                                                                      +
+// + enGrid is free software: you can redistribute it and/or modify       +
+// + it under the terms of the GNU General Public License as published by +
+// + the Free Software Foundation, either version 3 of the License, or    +
+// + (at your option) any later version.                                  +
+// +                                                                      +
+// + enGrid is distributed in the hope that it will be useful,            +
+// + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
+// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
+// + GNU General Public License for more details.                         +
+// +                                                                      +
+// + You should have received a copy of the GNU General Public License    +
+// + along with enGrid. If not, see <http://www.gnu.org/licenses/>.       +
+// +                                                                      +
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//
+
+#ifndef LOCALNODEGRAPHINTERFACE_H
+#define LOCALNODEGRAPHINTERFACE_H
+
+#include "meshpartition.h"
+
+/**
+ * @brief An interface to run generic algorithms on the node graph of a grid.
+ */
+class LocalNodeGraphInterface
+{
+
+  MeshPartition* m_Part;
+
+public:
+
+  typedef vtkIdType index_type;
+
+  void   setMeshPartition(MeshPartition* part) { m_Part = part; }
+  size_t size() { return m_Part->getNumberOfNodes(); }
+  size_t getNumLinks(size_t i) { return m_Part->n2nLSize(i); }
+  size_t getLink(size_t i, size_t j) { return m_Part->n2nLL(i,j); }
+
+};
+
+#endif // LOCALNODEGRAPHINTERFACE_H
