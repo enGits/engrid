c8cb0761de6ac0a65e3b85f4c1e3821896169a91
Oliver Gloth <ogloth@engits.com>
2014-07-16 09:02:44 +0200

    back to old version

--- src/libengrid/insertpoints.cpp
+++ src/libengrid/insertpoints.cpp
@@ -217,65 +217,30 @@ int InsertPoints::insertPoints()
 
 char InsertPoints::getNewNodeType(stencil_t S)
 {
-//   cout<<"S="<<S<<endl;
-
   vtkIdType id_node1 = S.p1;
   vtkIdType id_node2 = S.p2;
 
-  /*
-  if (S.id_cell.size() != 2) {
-    EG_ERR_RETURN("The surface mesh is not water-tight");
-  }
-  */
-  
   EG_VTKDCN(vtkCharArray, node_type, m_Grid, "node_type");
-  if (node_type->GetValue(id_node1) == EG_SIMPLE_VERTEX || node_type->GetValue(id_node2) == EG_SIMPLE_VERTEX ) {
+  if (node_type->GetValue(id_node1) == EG_SIMPLE_VERTEX || node_type->GetValue(id_node2) == EG_SIMPLE_VERTEX) {
     return EG_SIMPLE_VERTEX;
   } else {
-    if (m_AngleFeatureDefinition) {
-      if (node_type->GetValue(id_node1) > EG_SIMPLE_VERTEX && node_type->GetValue(id_node2) > EG_SIMPLE_VERTEX) {
-        if (S.id_cell.size() == 2) {
-          vec3_t n1 = cellNormal(m_Grid, S.id_cell[0]);
-          vec3_t n2 = cellNormal(m_Grid, S.id_cell[1]);
-          if (angle(n1, n2) >= m_FeatureAngle) {
-            return EG_FEATURE_EDGE_VERTEX;
-          }
-        }
-      }
-      return EG_SIMPLE_VERTEX;
-    } else {
-      QVector <vtkIdType> PSP = getPotentialSnapPoints(id_node1);
-      if( PSP.contains(id_node2) ) {
-        EG_VTKDCC(vtkIntArray, cell_code, m_Grid, "cell_code");
-        if (S.id_cell.size() < 1) {
-          if (m_AngleFeatureDefinition) {
-            return EG_FEATURE_EDGE_VERTEX;
-          }
-          return EG_BOUNDARY_EDGE_VERTEX;
-        } else if (S.id_cell.size() == 1) {
-          EG_ERR_RETURN("Invalid surface mesh. Check this with 'Tools -> Check surface integrity'.")
-              return EG_FEATURE_EDGE_VERTEX; //at best, this would be a feature edge, since it's loose.
-        } else {
-          if (cell_code->GetValue(S.id_cell[0]) != cell_code->GetValue(S.id_cell[1])) {
-            if (m_AngleFeatureDefinition) {
-              return EG_FEATURE_EDGE_VERTEX;
-            }
-            return EG_BOUNDARY_EDGE_VERTEX;
-          } else {
-
-            // count feature (edge and corner) vertices for node 1
-            // "cutting corners" might lead to wrong feature edge vertices
-            //return EG_FEATURE_EDGE_VERTEX;
-
-            if (m_Part.getNumberOfFeatureNeighbours(id_node1) == 2 && m_Part.getNumberOfFeatureNeighbours(id_node2) == 2) {
-              return EG_FEATURE_EDGE_VERTEX;
-            }
-            return EG_SIMPLE_VERTEX;
-          }
-        }
+    QVector <vtkIdType> PSP = getPotentialSnapPoints(id_node1);
+    if (PSP.contains(id_node2)) {
+      EG_VTKDCC(vtkIntArray, cell_code, m_Grid, "cell_code");
+      if (S.id_cell.size()<1) {
+        return EG_BOUNDARY_EDGE_VERTEX;
+      } else if (S.id_cell.size() == 1) {
+        EG_ERR_RETURN("Invalid surface mesh. Check this with 'Tools -> Check surface integrity'.");
+        return EG_FEATURE_EDGE_VERTEX; //at best, this would be a feature edge, since it's loose.
       } else {
-        return EG_SIMPLE_VERTEX;
+        if (cell_code->GetValue(S.id_cell[0]) != cell_code->GetValue(S.id_cell[1])) {
+          return EG_BOUNDARY_EDGE_VERTEX;
+        } else {
+          return EG_FEATURE_EDGE_VERTEX;
+        }
       }
+    } else {
+      return EG_SIMPLE_VERTEX;
     }
   }
 }
