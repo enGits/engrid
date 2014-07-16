6fb0817ebf6e6643c0b9da89e1ac27a615d1400e
Oliver Gloth <ogloth@engits.com>
2014-07-16 09:00:31 +0200

    skeleton for feature snap operation (edge sharpening)

---
+++ src/libengrid/snaptofeatures.h
@@ -0,0 +1,44 @@
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +                                                                      +
+// + This file is part of enGrid.                                         +
+// +                                                                      +
+// + Copyright 2008-2014 enGits GmbH                                      +
+// +                                                                      +
+// + enGrid is free software: you can redistribute it and/or modify       +
+// + it under the terms of the GNU General Public License as published by +
+// + the Free Software Foundation, either version 3 of the License, or    +
+// + (at your option) any later version.                                  +
+// +                                                                      +
+// + enGrid is distributed in the hope that it will be useful,            +
+// + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
+// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
+// + GNU General Public License for more details.                         +
+// +                                                                      +
+// + You should have received a copy of the GNU General Public License    +
+// + along with enGrid. If not, see <http://www.gnu.org/licenses/>.       +
+// +                                                                      +
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#ifndef SNAPTOFEATURES_H
+#define SNAPTOFEATURES_H
+
+class SnapToFeatures;
+
+#include "surfaceoperation.h"
+#include "cadinterface.h"
+
+class SnapToFeatures : public SurfaceOperation
+{
+
+protected: // methods
+
+  virtual void operate();
+
+
+public:
+
+  SnapToFeatures();
+
+};
+
+#endif // SNAPTOFEATURES_H
