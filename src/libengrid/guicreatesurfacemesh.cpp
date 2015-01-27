dedbae267dd986330b68472e400c3a2bd3b33155
Francois Thomas Pronk <ftpronk@engits.com>
2015-01-27 15:29:57 +0100

    Changed support date

--- src/libengrid/guicreatesurfacemesh.cpp
+++ src/libengrid/guicreatesurfacemesh.cpp
@@ -272,7 +272,7 @@ void GuiCreateSurfaceMesh::ClearAll_BC()
 
 void GuiCreateSurfaceMesh::setTextFromTable()
 {
-  EG_STOPDATE("2015-01-01");
+  EG_STOPDATE("2015-06-01");
   m_Ui.textEdit->setText(GuiMainWindow::pointer()->getXmlSection("engrid/surface/rules"));
   m_Ui.m_TextEditPrismaticLayers->setText(GuiMainWindow::pointer()->getXmlSection("engrid/blayer/rules"));
   return;
